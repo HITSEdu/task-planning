0fa6eb0bc2373d494ff3de6d5c3528ad3ebe60e8
YungOleg <bilk5899@gmail.com>
2026-01-06 16:14:01 +0700

    style: format project

--- src/app/_components/header/user-block.tsx
+++ src/app/_components/header/user-block.tsx
@@ -23,7 +23,7 @@ export default function UserBlock({ user }: UserBlockProps) {
         redirect("/sign-in");
       }
     }
-  }, [state])
+  }, [state]);
 
   return (
     <div className="flex items-center justify-between gap-4">
