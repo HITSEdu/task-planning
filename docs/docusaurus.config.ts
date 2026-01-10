4b5d2649b33166961ba03ae443d5e5cb837230a5
YungOleg <bilk5899@gmail.com>
2026-01-10 20:25:29 +0700

    ci: create deployments

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'http://localhost:3000',
+  url: 'https://gitlab.kupriyanov.space/kqkqkqkqkq/task-planning/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
