6cb45b4724a9a7e4d58a327c4f85b25dd4bb34a0
YungOleg <bilk5899@gmail.com>
2026-01-11 17:20:52 +0700

    ci: add code quality stage

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://gitlab.kupriyanov.space/kqkqkqkqkq/task-planning',
-  url: 'https://дщсфд',
+  url: 'https://task-planning-e732b2.pages.kupriyanov.space/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
